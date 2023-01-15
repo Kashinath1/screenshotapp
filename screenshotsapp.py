34cb7a51b6eb7637d606a3b2a5b08e866107f1c0
Kashinath11 <bken25gourkhali@gmail.com>
2023-01-15 08:27:41 +0545

    with_gui

--- screenshotsapp.py
+++ screenshotsapp.py
@@ -1,12 +1,29 @@
 import time
 import pyautogui
+import tkinter as tk
 
 def screenshot():
     name = int(round(time.time()* 1000))
     name = 'E:/python projects/screenshot folder/{}.png'.format(name)
-    time.sleep(5)
+    # time.sleep(5)
     img = pyautogui.screenshot(name)
     img.show()
     
 
-screenshot()
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(
+    frame,
+    text = "take screenshot",
+    command = screenshot)
+
+button.pack(side=tk.LEFT)
+close = tk.Button(
+    frame,
+    text = "quit",
+    command = quit)
+close.pack(side = tk.LEFT)
+
+root.mainloop()
