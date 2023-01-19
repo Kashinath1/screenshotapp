e14df580024ff6e7214c7505a6ea7598c1bc01ff
Kashinath11 <bken25gourkhali@gmail.com>
2023-01-19 08:59:00 +0545

    ocer_commit

---
+++ ocr.py
@@ -0,0 +1,13 @@
+import pytesseract
+import os
+from PIL import Image
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\tessaract\tesseract-ocr-w64-setup-5.3.0.20221222.exe"
+
+
+def convert():
+    img = Image.open('image.jpg')
+    text = pytesseract.image_to_string(img)
+    print(text)
+    
+convert()
